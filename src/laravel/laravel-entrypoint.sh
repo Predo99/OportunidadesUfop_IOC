b37c01d496642300a026656abdc53b2526d072d1
Lucas Lima <lucas.developmaster@gmail.com>
2020-08-09 02:01:32 -0300

    Finalizando outra faze de testes no sistema

--- src/laravel/laravel-entrypoint.sh
+++ src/laravel/laravel-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+chmod 777 -R /app
+echo -e "\e[1;31;42m Mudando as permissoes da pasta do laravel! \e[0m";
+
 echo -e "\e[1;31;42m Installing/Updating Laravel dependencies (composer)! \e[0m";
 if [[ ! -f /app/vendor ]]; then
   composer install
@@ -11,11 +14,11 @@ else
   echo -e "\e[1;31;42m Dependencies updated! \e[0m";
 fi
 
-echo -e "\e[1;31;42m Gerando chave do laravel! \e[0m";
 php artisan key:generate
+echo -e "\e[1;31;42m Gerando chave do laravel! \e[0m";
 
-echo -e "\e[1;31;42m Lipando o cache de configuracoes do sistema e reconfigurando! \e[0m";
 php artisan config:clear
 php artisan config:cache
+echo -e "\e[1;31;42m Lipando o cache de configuracoes do sistema e reconfigurando! \e[0m";
 
 exec "$@";
