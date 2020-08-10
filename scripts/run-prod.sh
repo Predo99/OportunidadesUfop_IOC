c81a5b4fe8abed42b8e600a62c7ad68b7b836d19
Lucas Lima <lucas.developmaster@gmail.com>
2020-08-09 22:33:42 -0300

    Criando scripts de inicializacao

---
+++ scripts/run-prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]; then
+    echo ""
+    echo -e "\e[1;32;41m Please run this script with sudo: \e[0m"
+    echo ""
+    echo -e "\e[1;32;41m sudo $0 $* \e[0m"
+    exit 1
+fi
+
+docker-compose stop && docker-compose up -d
